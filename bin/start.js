5260232f769a5e91b2a22fefffb2414194d940e3
wyster <wyster@make.im>
2023-12-12 08:48:25 -0800

    run webserver

---
+++ bin/start.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const Express = require('express');
+const Router = require('express-promise-router');
+
+const router = Router()
+
+const buildDir = path.join(__dirname, '/../build')
+
+const app = new Express()
+app.use(router)
+router.use('/static', Express.static(path.join(buildDir, '/static')))
+
+const port = process.env.PORT || 80
+app.listen(port, () => console.log(`app Server is now running on http://localhost:${port}`))
