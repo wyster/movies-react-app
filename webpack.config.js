8bf995903b8408cdf3b5c9f72a0599a652dabcdb
wyster <wyster@make.im>
2023-12-17 08:54:26 -0800

    add support for chrome 30

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,10 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyPlugin = require("copy-webpack-plugin");
 
+const transpileDependencies = [
+  '@apollo'
+]
+
 module.exports = {
   entry: "./src/index.js",
   output: {
@@ -11,15 +15,27 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx'],
   },
+  //devtool: 'source-map',
+  devtool: false,
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
+        include: [
+          path.resolve('src'),
+        ],
         use: {
           loader: "babel-loader",
           options:{
             presets: [
+              [
+                "@babel/preset-env",
+                {
+                  useBuiltIns: 'usage',
+                  "corejs": 3,
+                  debug: true,
+                }
+              ],
               [
                 "@babel/preset-react",
                 {
@@ -27,6 +43,61 @@ module.exports = {
                 }
               ]
             ],
+          }
+        },
+      },
+      {
+        test: /\.(js)$/,
+        include: [
+          path.resolve('node_modules/apollo-link-rest'),
+          path.resolve('node_modules/@sentry'),
+          path.resolve('node_modules/yup'),
+          path.resolve('node_modules/react'),
+        ],
+        use: {
+          loader: "babel-loader",
+          options:{
+            presets: [
+              [
+                "@babel/preset-env",
+                {
+                  useBuiltIns: 'usage',
+                  "corejs": 3,
+                  debug: true,
+                  modules: "cjs"
+                }
+              ],
+              [
+                "@babel/preset-react",
+                {
+                  "runtime": "automatic"
+                }
+              ]
+            ],
+          }
+        },
+      },
+      {
+        test: /\.(js|mjs)$/,
+        include: [
+          path.resolve('node_modules/@apollo'),
+          path.resolve('node_modules/optimism'),
+          path.resolve('node_modules/@wry'),
+          path.resolve('node_modules/ts'),
+        ],
+        use: {
+          loader: "babel-loader",
+          options:{
+            presets: [
+              [
+                "@babel/preset-env",
+                {
+                  useBuiltIns: 'usage',
+                  "corejs": 3,
+                  debug: true,
+                }
+              ]
+            ]
           }
         },
       },
