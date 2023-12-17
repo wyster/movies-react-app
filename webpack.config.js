50d416a8ad607dfb92847d40d90af7fad4d36423
wyster <wyster@make.im>
2023-12-16 18:40:26 -0800

    add corejs

--- webpack.config.js
+++ webpack.config.js
@@ -20,8 +20,19 @@ module.exports = {
           loader: "babel-loader",
           options:{
             presets: [
-              "@babel/preset-env",
-              ["@babel/preset-react", {"runtime": "automatic"}]
+              [
+                "@babel/preset-env",
+                {
+                  useBuiltIns: 'usage',
+                  "corejs": 3
+                }
+              ],
+              [
+                "@babel/preset-react",
+                {
+                  "runtime": "automatic"
+                }
+              ]
             ],
           }
         },
