ade26d866a21eeaf93a097b3d57344b857fc2212
wyster <wyster@make.im>
2023-12-17 05:57:32 -0800

    use corejs

--- webpack.config.js
+++ webpack.config.js
@@ -21,13 +21,6 @@ module.exports = {
           options:{
             presets: [
               [
-                "@babel/preset-env",
-                {
-                  useBuiltIns: 'usage',
-                  "corejs": 3
-                }
-              ],
-              [
                 "@babel/preset-react",
                 {
                   "runtime": "automatic"
