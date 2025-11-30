3d50a2a432400733dd5a35693ef4ae65d60e4445
Illia Zelenin <wyster@make.im>
2025-11-30 14:28:28 -0800

    add navigation

--- src/routes/Layout.js
+++ src/routes/Layout.js
@@ -1,15 +1,20 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from "react-router"
+import {BrowserRouter, Routes, Route, NavLink} from "react-router"
 
 import Main from '../views/Main'
 import Movie from "../views/Movie";
 
 const Layout = () =>
   <div className="container-fluid mt-3 mb-3">
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/movie/:id" element={<Movie />} />
-      </Routes>
+    <nav aria-label="breadcrumb">
+      <ol className="breadcrumb">
+        <li className="breadcrumb-item"><NavLink to="/">Home</NavLink></li>
+      </ol>
+    </nav>
+    <Routes>
+      <Route path="/" element={<Main/>}/>
+      <Route path="/movie/:id" element={<Movie/>}/>
+    </Routes>
   </div>
 
 export default Layout
