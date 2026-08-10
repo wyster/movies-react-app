d8ca685fddb99769d34940271c4f43d14a34ea4d
Illia Zelenin <wyster@make.im>
2026-08-10 15:28:30 +0200

    update videojs

--- src/components/Video/Player.jsx
+++ src/components/Video/Player.jsx
@@ -1,12 +1,10 @@
 import { useEffect, useRef, useState } from 'react'
-import video from 'video.js';
-import 'video.js/dist/video-js.css';
-import 'videojs-hotkeys';
-import { useVideoJS } from "react-hook-videojs";
 import Cast from '../../utils/Cast';
 import gql from "graphql-tag";
 import {useQuery} from "@apollo/client/react";
 import useCast from "../../hooks/cast";
+import { createPlayer, videoFeatures } from '@videojs/react';
+import { VideoSkin, Video } from '@videojs/react/video';
 
 const GET_MOVIE_DETAILS = gql`
   query MovieDetails($id: Number) {
@@ -28,45 +26,12 @@ function Player ({
   onEnded = () => {},
   autoPlay = false
 }) {
+  const Player = createPlayer({ features: videoFeatures });
   const videoElement = useRef()
   const videoContainer = useRef()
   const [timer, setTimer] = useState(null)
   const {cast: myCastJs, myCast, setCast} = useCast();
   const { loading, error, data: movieData } = useQuery(GET_MOVIE_DETAILS, { variables: { id: movieId } })
-
-  const { Video, player, ready } = useVideoJS(
-    { sources: [{ src: src }] },
-  );
-
-  if (ready) {
-    player.currentTime(currentTime);
-  }
-
-  function timeUpdate (e) {
-    if (!ready || myCastJs?.state === 'playing') {
-      return;
-    }
-    const value = parseInt(e.target.currentTime, 10)
-    setTimer(current => {
-      if (value === current) {
-        return current
-      }
-
-      return value
-    })
-  }
-
-  function volumeChange (e) {
-    onChangeVolume(parseInt(e.target.volume * 100, 10))
-  }
-
-  function ended (e) {
-    onEnded(parseInt(e.target.currentTime, 10))
-  }
-
-  function requestFullScreen()  {
-    player.requestFullscreen();
-  }
 
   function cast()  {
     if (myCast.connected) {
@@ -117,43 +82,6 @@ function Player ({
   }
 
   useEffect(() => {
-    if (!player) {
-      return;
-    }
-
-    if (autoPlay) {
-      player.play();
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [player])
-
-  useEffect(() => {
-    if (!videoElement.current || !videoElement.current.paused || !myCast.connected) {
-      return
-    }
-    if (!currentTime) {
-      return;
-    }
-    videoElement.current.currentTime = currentTime
-  }, [currentTime])
-
-  useEffect(() => {
-    if (timer === null) {
-      return
-    }
-
-    onCurrentTimeChange(timer)
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [timer])
-
-  useEffect(() => {
-    if (volume === null || !videoElement.current) {
-      return
-    }
-    videoElement.current.volume = volume / 100
-  }, [volume])
-
-  useEffect(() => {
     if (myCast.paused) {
       setTimer(myCast.timer);
     }
@@ -180,16 +108,6 @@ function Player ({
 
   return (
     <>
-      <button
-        type="button"
-        className={`btn`}
-        onClick={e => {
-          e.preventDefault();
-          requestFullScreen()
-        }}
-      >
-        Fullscreen
-      </button>
       <button
         type="button"
         className={`btn`}
@@ -244,14 +162,11 @@ function Player ({
         )}
       </div>
       <div ref={videoContainer}>
-        <Video
-          ref={videoElement}
-          className="video-js"
-          controls
-          onEnded={ended}
-          //onTimeUpdate={timeUpdate}
-          onVolumeChange={volumeChange}
-        />
+        <Player.Provider>
+          <VideoSkin>
+            <Video src={src} playsInline />
+          </VideoSkin>
+        </Player.Provider>
       </div>
     </>
   )
