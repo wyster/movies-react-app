3f1bef25350459602ac1a21a88acb4ddbe09e71d
Illia Zelenin <wyster@make.im>
2026-08-18 14:24:19 +0200

    add webpack cache

--- webpack.config.js
+++ webpack.config.js
@@ -3,78 +3,94 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, "build"),
-    publicPath: '/',
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  //devtool: 'source-map',
-  devtool: false,
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        include: [
-          path.resolve('src'),
-        ],
-        use: [
-          {
-            loader: "babel-loader",
-            options: {
-              presets: [
-                [
-                  "@babel/preset-env"
-                ],
-                [
-                  "@babel/preset-react",
-                  {
-                    "runtime": "automatic"
-                  }
-                ]
-              ],
-            }
-          },
-          'astroturf/loader'
-        ]
-      },
-      {
-        test: /\.css$/i,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          "postcss-loader"
-        ],
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          "postcss-loader",
-          "sass-loader",
-        ],
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "src", "index.html")
-    }),
-    new CopyPlugin({
-      patterns: [
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+  return {
+    entry: "./src/index.js",
+    output: {
+      path: path.resolve(__dirname, "build"),
+      publicPath: '/',
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+    //devtool: 'source-map',
+    devtool: false,
+    module: {
+      rules: [
         {
-          from: path.resolve(__dirname, "public"),
-          to: path.resolve(__dirname, "build")
-        }
+          test: /\.(js|jsx)$/,
+          include: [
+            path.resolve('src'),
+          ],
+          use: [
+            {
+              loader: "babel-loader",
+              options: {
+                presets: [
+                  [
+                    "@babel/preset-env"
+                  ],
+                  [
+                    "@babel/preset-react",
+                    {
+                      "runtime": "automatic"
+                    }
+                  ]
+                ],
+              }
+            },
+            'astroturf/loader'
+          ]
+        },
+        {
+          test: /\.css$/i,
+          use: [
+            MiniCssExtractPlugin.loader,
+            "css-loader",
+            "postcss-loader"
+          ],
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            MiniCssExtractPlugin.loader,
+            "css-loader",
+            "postcss-loader",
+            "sass-loader",
+          ],
+        },
       ],
-    }),
-    new MiniCssExtractPlugin()
-  ],
-  devServer: {
-    historyApiFallback: true,
-  }
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "src", "index.html")
+      }),
+      new CopyPlugin({
+        patterns: [
+          {
+            from: path.resolve(__dirname, "public"),
+            to: path.resolve(__dirname, "build")
+          }
+        ],
+      }),
+      new MiniCssExtractPlugin()
+    ],
+    devServer: {
+      historyApiFallback: true,
+    },
+    cache: {
+      type: 'filesystem',
+      name: isProduction ? 'frontend-production' : 'frontend-development',
+      version: JSON.stringify({
+        nodeEnv: process.env.NODE_ENV,
+      }),
+      cacheDirectory: path.resolve(__dirname, '.cache/webpack/frontend'),
+      buildDependencies: {
+        config: [
+          __filename,
+        ],
+      },
+    }
+  };
 };
